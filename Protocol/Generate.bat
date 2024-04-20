da51528ec2fbfcaead1e3660061be649fc58cd5a
AmaneKanata <85724878+AmaneKanata@users.noreply.github.com>
2024-04-20 12:45:16 +0900

    자동 생성 코드 생성 위치 변경

--- Protocol/Generate.bat
+++ Protocol/Generate.bat
@@ -5,24 +5,27 @@ SETLOCAL
 set ROOT_DIR=%~dp0
 set PROTO_PATH=%ROOT_DIR%src\ProtoFiles
 set TEMPLATE_PATH=%ROOT_DIR%src\Templates
-set OUTPUT_PATH=%ROOT_DIR%output
+set OUTPUT_PATH_CLIENT=%ROOT_DIR%..\Client\Assets\Scripts\Network\Core\Generated
+set OUTPUT_PATH_SERVER=%ROOT_DIR%..\Server\Assets\Scripts\Network\Core\Generated
 set PROTOC=%ROOT_DIR%src\protoc.exe
 set GENERATOR=%ROOT_DIR%src\Generator.exe
 
 :: Ensure output directory exists
-if not exist %OUTPUT_PATH% mkdir %OUTPUT_PATH%
+if not exist %OUTPUT_PATH_CLIENT% mkdir %OUTPUT_PATH_CLIENT%
+if not exist %OUTPUT_PATH_SERVER% mkdir %OUTPUT_PATH_SERVER%
 
 :: Compile .proto files to C# source code
 echo Compiling .proto files to C#...
 for %%f in (%PROTO_PATH%\*.proto) do (
     echo Processing %%~nxf
-    %PROTOC% -I=%PROTO_PATH% --csharp_out=%OUTPUT_PATH% %%f
+    %PROTOC% -I=%PROTO_PATH% --csharp_out=%OUTPUT_PATH_CLIENT% %%f
+    %PROTOC% -I=%PROTO_PATH% --csharp_out=%OUTPUT_PATH_SERVER% %%f
 )
 
 :: Run the Packet Generator
 echo Running Packet Generator...
-%GENERATOR% --proto_path "%PROTO_PATH%" --template_path "%TEMPLATE_PATH%" --output_path "%OUTPUT_PATH%\Server" --recv "C_" --send "S_"
-%GENERATOR% --proto_path "%PROTO_PATH%" --template_path "%TEMPLATE_PATH%" --output_path "%OUTPUT_PATH%\Client" --recv "S_" --send "C_"
+%GENERATOR% --proto_path "%PROTO_PATH%" --template_path "%TEMPLATE_PATH%" --output_path "%OUTPUT_PATH_CLIENT%" --recv "S_" --send "C_"
+%GENERATOR% --proto_path "%PROTO_PATH%" --template_path "%TEMPLATE_PATH%" --output_path "%OUTPUT_PATH_SERVER%" --recv "C_" --send "S_"
 
 echo All tasks completed successfully.
 ENDLOCAL
